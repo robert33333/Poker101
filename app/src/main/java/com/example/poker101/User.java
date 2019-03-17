a455fef2f325caf159b0ba9dc6bf6a5eb32aef4c
Robert <robertgr991@yahoo.ro>
2019-03-18 00:32:51 +0200

    Finalized log in and accessing data with Facebook API (friends, id, profile picture etc.)

---
+++ app/src/main/java/com/example/poker101/User.java
@@ -0,0 +1,12 @@
+package com.example.poker101;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+
+import org.json.JSONObject;
+
+public class User {
+    public static JSONObject user;
+    public static AccessToken accessToken;
+    public static CallbackManager callbackManager;
+}
