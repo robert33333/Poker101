0c567088346a95d81d574bcb36724a9ffb780b28
Robert <robertgr991@yahoo.ro>
2019-03-18 00:31:39 +0200

    Finalized log in and accessing data with Facebook API (friends, id, profile picture etc.)

---
+++ app/src/main/java/com/example/poker101/MenuFragment.java
@@ -0,0 +1,42 @@
+package com.example.poker101;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.json.JSONException;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MenuFragment extends Fragment {
+
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            User.user.get("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
